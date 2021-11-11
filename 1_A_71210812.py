512540fe24758d63bd9d7f83b4ad932f66ed2b5f
Janice <janice.ivena@ti.ukdw.ac.id>
2021-11-11 23:00:14 +0700

    soal 1

---
+++ 1_A_71210812.py
@@ -0,0 +1,11 @@
+#input diameter
+
+diameter = int(input("Masukan diameter : "))
+
+
+#proses
+Keliling = diameter * 3.14
+Hasil = round(Keliling,2)
+
+#print
+print("Keliling lingkaran adalah : ", Hasil)
