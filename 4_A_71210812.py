c31de99d98da133c63a55a0536ca5f604a3f257f
Janice <janice.ivena@ti.ukdw.ac.id>
2021-11-11 23:27:16 +0700

    soal 4

--- 4_A_71210812.py
+++ 4_A_71210812.py
@@ -7,16 +7,16 @@ es = int(input("ES DOGER           Rp 6.000,00   : "))
 rujak = int(input("RUJAK CINGUR       Rp 8.000,00   : "))
 
 #proses
-ikann = ikan * 25000
-ess = es * 6000
-rujakk = rujak * 8000
-total = ikann + ess + rujakk
+ikanb = ikan * 25000
+esd = es * 6000
+rujakc = rujak * 8000
+total = ikanb + esd + rujakc
 
 #print
 print ("=====TOTAL=====")
-print ("TOTAL IKAN BAKAR     : Rp", ikann)
-print ("TOTAL ES DOGER       : Rp", ess)
-print ("TOTAL RUJAK CINGUR   : Rp", rujakk)
+print ("TOTAL IKAN BAKAR     : Rp", ikanb)
+print ("TOTAL ES DOGER       : Rp", esd)
+print ("TOTAL RUJAK CINGUR   : Rp", rujakc)
 print ("Jumlah total biaya yang harus dibayar adalah Rp", total)
 
 
