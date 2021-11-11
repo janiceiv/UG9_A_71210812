e66d76d06ad1fded0906d6d344abcf01ad64ef87
Janice <janice.ivena@ti.ukdw.ac.id>
2021-11-11 23:12:17 +0700

    soal 3

---
+++ 3_A_71210812.py
@@ -0,0 +1,10 @@
+#input produk
+
+produk1 = (input("Masukan daftar belanja 1: "))
+produk2 = (input("Masukan daftar belanja 2: "))
+tambahan1 = (input("Tambahkan data ke daftar belanja 1: "))
+tambahan2 = (input("Tambahkan data ke daftar belanja 21: "))
+
+#print
+print("Daftar belanja 1 adalah", produk1,",", tambahan1)
+print("Daftar belanja 2 adalah", produk2,",", tambahan2)
